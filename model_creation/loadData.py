886f9149cb7faefdc6072a113140857e377c9ac2
LBH97 <58932537+MoonLBH@users.noreply.github.com>
2022-06-07 15:03:37 +0800

    Update loadData.py

--- model_creation/loadData.py
+++ model_creation/loadData.py
@@ -6,12 +6,10 @@ def loadGlove(inputpath, outputpath=""):
     wordEmb = {}
     with open(inputpath) as f:
         for line in f:
-            # 基本的数据整理
             ll = line.strip().split(',')
             ll[0] = str(int(float(ll[0])))
             data_list.append(ll)
 
-            # 构建wordembeding的选项
             ll_new = [float(i) for i in ll]
             emb = np.array(ll_new[1:], dtype="float32")
             wordEmb[str(int(ll_new[0]))] = emb
@@ -45,21 +43,5 @@ def loadData(inputpath):
             # print(label_item)
             label.append(label_item)
     return data_list, label
-
-
-#
-# inputpath = "data/position_data_2_1.txt"
-# # outputpath = "keras_model_glovepositionmatrix.csv"
-# # data_list = loadGlove(inputpath)
-# # # data_list = pd.DataFrame(data_list)
-# # # data_list.to_csv('keras_model_GloVeVec_3_1_2_150_20000.csv', index=None)
-# # print(data_list['11:13'])
-# #
-# # inputpath = "data/position_data_2_1.txt"
-# data_list,label = loadData(inputpath)
-# print(data_list[0:2])
-# print(label[0:2])
-
-
 
 
